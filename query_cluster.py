9e6b2e1bbc7c1ec76e2304cac684281c51854c17
Rodrigo Daguano <rfdaguano2@gmail.com>
2021-06-27 22:11:50 -0300

    Question 3 solved

--- query_cluster.py
+++ query_cluster.py
@@ -21,9 +21,16 @@ queries = [
     order by total desc
     limit 3;""",
 
-    """select count(*) as Count, Datepart(Month,pickup_datetime) as Month, Datepart(Year, pickup_datetime) as Year from trips
+    """select count(*) as Cash_Only, Datepart(Month,pickup_datetime) as Month, Datepart(Year, pickup_datetime) as Year from trips
     join payment on payment.payment_type=trips.payment_type
     where payment_lookup = 'Cash'
+    group by Month, Year
+    order by Year, Month
+    ;""",
+
+    """select count(*) as Not_Cash, Datepart(Month,pickup_datetime) as Month, Datepart(Year, pickup_datetime) as Year from trips
+    join payment on payment.payment_type=trips.payment_type
+    where payment_lookup != 'Cash'
     group by Month, Year
     order by Year, Month
     ;""",
@@ -46,38 +53,62 @@ queries = [
     where date_part(weekday, pickup_datetime) in (6, 0);"""
 ]
 
+# Question 1
 question1 = pd.read_sql(queries[0], engine)
 question1.to_csv('Results/question1.csv', index=False)
 question1 = pd.read_csv('Results/question1.csv')
 print(question1)
 
+# Question 2
 question2 = pd.read_sql(queries[1], engine)
 question2.to_csv('Results/question2.csv', index=False)
 question2 = pd.read_csv('Results/question2.csv')
 print(question2)
 
-question3 = pd.read_sql(queries[2], engine)
-question3.to_csv('Results/question3.csv', index=False)
+# Question 3
+question3a = pd.read_sql(queries[2], engine)
+question3a.to_csv('Results/question3a.csv', index=False)
+question3a = pd.read_csv('Results/question3a.csv', index_col=['month', 'year'])
+
+question3b = pd.read_sql(queries[3], engine)
+question3b.to_csv('Results/question3b.csv', index=False)
+question3b = pd.read_csv('Results/question3b.csv', index_col=['month', 'year'])
+
+question3 = question3a.join(question3b, on=['month', 'year'])
+question3.to_csv('Results/question3.csv')
 question3 = pd.read_csv('Results/question3.csv', index_col=['month', 'year'])
-question3.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
+
+question3.plot(kind='bar', title='Number of rides paid with and without cash', xlabel='(Month, Year)', ylabel='Count')
+plt.tight_layout()
 plt.savefig('Results/question3.png')
+
+# question3a.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
+# plt.savefig('Results/question3a.png')
+
+# question3b.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
+# plt.savefig('Results/question3b.png')
+
 print(question3)
 
-question4a = pd.read_sql(queries[3], engine)
+# Question 4
+question4a = pd.read_sql(queries[4], engine)
 question4a.to_csv('Results/question4a.csv', index=False)
 question4a = pd.read_csv('Results/question4a.csv', index_col='date')
 question4a.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
 plt.savefig('Results/question4a.png')
 print(question4a)
 
-question4b = pd.read_sql(queries[4], engine)
+question4b = pd.read_sql(queries[5], engine)
 question4b.to_csv('Results/question4b.csv', index=False)
 question4b = pd.read_csv('Results/question4b.csv', index_col=['day', 'month'])
 question4b.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
 plt.savefig('Results/question4b.png')
 print(question4b)
 
-question5 = pd.read_sql(queries[5], engine)
+# Question 5
+question5 = pd.read_sql(queries[6], engine)
 question5.to_csv('Results/question5.csv', index=False)
 question5 = pd.read_csv('Results/question5.csv')
 print(question5)
+
+#TODO Extra Geolocation
