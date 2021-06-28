956febae35ebf2cf46240efcd84ac74e6e9cb289
Rodrigo Daguano <rfdaguano2@gmail.com>
2021-06-27 21:38:30 -0300

    Queries work and there are two alternatives for indexing the table for question 3

--- query_cluster.py
+++ query_cluster.py
@@ -45,6 +45,7 @@ queries = [
     """select avg(datediff(seconds, pickup_datetime, dropoff_datetime)) as Average_sec from trips
     where date_part(weekday, pickup_datetime) in (6, 0);"""
 ]
+
 question1 = pd.read_sql(queries[0], engine)
 question1.to_csv('Results/question1.csv', index=False)
 question1 = pd.read_csv('Results/question1.csv')
@@ -58,19 +59,9 @@ print(question2)
 question3 = pd.read_sql(queries[2], engine)
 question3.to_csv('Results/question3.csv', index=False)
 question3 = pd.read_csv('Results/question3.csv', index_col=['month', 'year'])
-# q_3 = question3.set_index(['month', 'year'])
-# ix = pd.MultiIndex.from_frame(question3[['month', 'year']])
-# print(ix)
-# q3 = pd.DataFrame({'count': question3['count']}, index=ix)
-# print(q_3)
 question3.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
 plt.savefig('Results/question3.png')
 print(question3)
-# m_q3 = pd.MultiIndex.from_frame(question3)
-# m_q3.plot.bar()
-# plt.savefig('question3.png')
-# fig.savefig('test.pdf')
-
 
 question4a = pd.read_sql(queries[3], engine)
 question4a.to_csv('Results/question4a.csv', index=False)
@@ -85,7 +76,6 @@ question4b = pd.read_csv('Results/question4b.csv', index_col=['day', 'month'])
 question4b.plot(kind='bar', title='Number of rides with tips', xlabel='x_label', ylabel='y_label')
 plt.savefig('Results/question4b.png')
 print(question4b)
-
 
 question5 = pd.read_sql(queries[5], engine)
 question5.to_csv('Results/question5.csv', index=False)
